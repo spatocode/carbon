5a98d70c468991089c892fdd18f78327a7132ea9
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-03 19:14:45 +0100

    encapsulate menu and view into one component
    Also update menu component design

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,29 +1,75 @@
 import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
+import { selectView, nightMode } from "../actions"
+import { version } from "../../package.json"
+import "./stylesheets/Menu.scss"
 
-const Menu = ({ onNowPlaying=f=>f, onPlayists=f=>f }) => {
+const Menu = ({ nightMode=false, handleView=f=>f }) => {
+    const handleNightmode = () => {
+        handleView(!nightMode)
+    }
     return (
         <div className="Menu">
-            <div className="now-playing" onClick={onNowPlaying}>Now Playing</div>
-            <div className="playists" onClick={onPlayists}>Playists</div>
-            <div className="music">Music</div>
-            <div className="video">Video</div>
+            <div className="title">
+                <img src=""/>
+                {version}
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Now Playing</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span><img src=""/></span>
+                <span>Music</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Video</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Playist</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Favourite</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Downloads</span>
+            </div>
+            <div className="menu-list" onClick={handleView}>
+                <span></span>
+                <span>Settings</span>
+            </div>
+            <div className="menu-list" onClick={handleNightmode}>
+                <span>Night mode</span>
+                <div className="mode-toggle-bar">
+                    <div className="mode-toggle" style={nightMode ? { float: "right" } : { float: "left" }}></div>
+                </div>
+            </div>
         </div>
     )
 }
 
 Menu.propTypes = {
-    onNowPlaying: PropTypes.func,
-    onPlayists: PropTypes.func
+    handleView: PropTypes.func,
+    handleNightmode: PropTypes.func
 }
 
-const mapStateToProps = () => ({
-
+const mapStateToProps = (state) => ({
+    nightMode: state.mode.night
 })
 
-const mapDispatchToProps = () => ({
-
+const mapDispatchToProps = (dispatch) => ({
+    handleView: (arg) => {
+        if (typeof arg === "boolean") {
+            dispatch(nightMode(arg))
+        } else {
+            dispatch(selectView(arg.currentTarget.innerText))
+        }
+    }
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Menu)
