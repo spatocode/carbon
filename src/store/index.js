2851fb6ab5f2113bd84a43f3b187cce9dff48425
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-05 12:35:10 +0100

    lint code

--- src/store/index.js
+++ src/store/index.js
@@ -5,36 +5,36 @@ import * as reducers from "../reducers"
  * Logs state and action to the console
  */
 const logger = store => next => action => {
-  let result
-  console.groupCollapsed('dispatching', action.type)
-  console.log('prev state', store.getState())
-  console.log('action', action)
-  result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd()
+    let result
+    console.groupCollapsed("dispatching", action.type)
+    console.log("prev state", store.getState())
+    console.log("action", action)
+    result = next(action)
+    console.log("next state", store.getState())
+    console.groupEnd()
 }
 
 /**
  * Saves the state in the localStorage
  */
 const saver = store => next => action => {
-  let result = next(action)
-  localStorage["redux-store"] = JSON.stringify(store.getState())
-  return result
+    const result = next(action)
+    localStorage["redux-store"] = JSON.stringify(store.getState())
+    return result
 }
 
 /**
  * Returns the application state tree
- * @param {Object} preloadedState 
+ * @param {Object} preloadedState
  */
 const storeFactory = (preloadedState) =>
-  createStore(
-    combineReducers(reducers),
-    preloadedState,
-    applyMiddleware(
-      saver,
-      logger
+    createStore(
+        combineReducers(reducers),
+        preloadedState,
+        applyMiddleware(
+            saver,
+            logger
+        )
     )
-  )
 
 export default storeFactory
