db10000cb4672c21cff29472a80a56b5da6d2779
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-06 14:29:19 +0100

    connect dumb components to the states & dispatcher

--- src/components/Artist.jsx
+++ src/components/Artist.jsx
@@ -1,25 +1,39 @@
 import React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
 
-const Artist = () => {
-    return (
-        <div className="tab-pane" id="Artist">
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Artist</th>
-                        <th>Count</th>
-                        <th>Length</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+const Artist = ({ tab="All", songs=[] }) => {
+    if (tab === "Artist") {
+        console.log(tab)
+        return (
+            <div className="tab-view" id="Artist">
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Artist</th>
+                            <th>Count</th>
+                            <th>Length</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+    return null
 }
 
-export default Artist
+Artist.propTypes = {
+    tab: PropTypes.string
+}
+
+const mapStateToProps = (state) => ({
+    tab: state.view.tab
+})
+
+export default connect(mapStateToProps, null)(Artist)
--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -1,25 +1,38 @@
 import React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
 
-const Genre = () => {
-    return (
-        <div className="tab-pane" id="Genre">
-            <table className="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Genre</th>
-                        <th>Count</th>
-                        <th>Length</th>
-                        <th>Rating</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+const Genre = ({ tab="All", songs=[] }) => {
+    if (tab === "Genre") {
+        return (
+            <div className="tab-view" id="Genre">
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Genre</th>
+                            <th>Count</th>
+                            <th>Length</th>
+                            <th>Rating</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+    return null
 }
 
-export default Genre
+Genre.propTypes = {
+    tab: PropTypes.string
+}
+
+const mapStateToProps = (state) => ({
+    tab: state.view.tab
+})
+
+export default connect(mapStateToProps, null)(Genre)
