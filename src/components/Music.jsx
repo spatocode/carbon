f22fc28d509078103e113baa55efc3f3f0283c3b
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-06 14:30:17 +0100

    update component to include tab buttons

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,16 +1,23 @@
 import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
+import { selectTab } from "../actions"
 import All from "./All"
 import Artist from "./Artist"
 import Album from "./Album"
 import Genre from "./Genre"
 import "./stylesheets/Music.scss"
 
-const Music = ({ songs=[] }) => {
+const Music = ({ tab="All", selectTab=f=>f }) => {
     return (
         <div className="View">
             <div className="music">
+                <div className="tab">
+                    <div className="tab-item" onClick={selectTab}>All</div>
+                    <div className="tab-item" onClick={selectTab}>Artist</div>
+                    <div className="tab-item" onClick={selectTab}>Album</div>
+                    <div className="tab-item" onClick={selectTab}>Genre</div>
+                </div>
                 <div className="tab-content">
                     <All />
                     <Artist />
@@ -23,15 +30,16 @@ const Music = ({ songs=[] }) => {
 }
 
 Music.propTypes = {
-    song: PropTypes.array
+    tab: PropTypes.string,
+    selectTab: PropTypes.func
 }
 
 const mapStateToProps = (state) => ({
-
+    tab: state.view.tab
 })
 
 const mapDispatchToProps = (dispatch) => ({
-
+    selectTab: (e) => dispatch(selectTab(e.currentTarget.innerText))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Music)
