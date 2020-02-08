66350a15244c72669146c3c272e078d440f35d36
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-08 18:42:54 +0100

    add more needed property to the browser window
    Also differentiate between dev and prod environment when loading web contents. Update and lint code to make sure eslint is happy.

--- main/index.js
+++ main/index.js
@@ -1,39 +1,55 @@
+const path = require("path")
 const { app, BrowserWindow } = require("electron")
+const buildMenu = require("./menu")
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
-  app.quit()
+    app.quit()
 }
 
-let mainWindow
+let window
 
 const createWindow = () => {
-  mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 630
-  })
+    const url = process.env.ELECTRON_ENV === "development"
+        ? path.join(__dirname, "/../build/index.html") : "http://localhost:3000"
 
-  mainWindow.loadURL("http://localhost:3000")
+    window = new BrowserWindow({
+        title: "Carbon Media Player",
+        show: false,
+        width: 1000,
+        height: 600,
+        useContentSize: true,
+        minHeight: 400,
+        minWidth: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            preload: path.join(__dirname, "/preload.js")
+        }
+    })
 
-  mainWindow.on("closed", () => {
-    mainWindow = null
-  })
+    window.loadURL(url)
 
-  mainWindow.webContents.on("dom-ready", () => {
-    mainWindow.show()
-  })
+    buildMenu(window)
+
+    window.on("closed", () => {
+        window = null
+    })
+
+    window.webContents.on("dom-ready", () => {
+        window.show()
+    })
 }
 
 app.on("ready", createWindow)
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit()
-  }
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
 })
 
 app.on("activate", () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (window === null) {
+        createWindow()
+    }
 })
