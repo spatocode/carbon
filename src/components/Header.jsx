30d8d0720e1de03c679495067c5e0547aedff8c4
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-10 15:53:00 +0100

    clean header component
    No need to display playing song.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from "react"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import { searchSong } from "../actions"
+import "./stylesheets/Header.scss"
 
-const Header = ({ song="", handleSearch=f=>f }) => {
+const Header = ({ handleSearch=f=>f }) => {
     return (
         <div className="Header">
-            <div className="now-playing-song">{song}</div>
             <div className="music-search">
                 <form onSubmit={handleSearch}>
-                    <button type="submit" onClick={handleSearch}></button>
+                    <button type="submit" onClick={handleSearch}>S</button>
                     <input type="search" placeholder="Search music" onChange={handleSearch} />
                 </form>
             </div>
@@ -18,17 +18,12 @@ const Header = ({ song="", handleSearch=f=>f }) => {
 }
 
 Header.propTypes = {
-    song: PropTypes.string,
     handleSearch: PropTypes.func,
     handleChange: PropTypes.func
 }
-
-const mapStateToProps = state => ({
-    song: state.playState.song
-})
 
 const mapDispatchToProps = dispatch => ({
     handleSearch: (e) => dispatch(searchSong(e.target.value))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default connect(null, mapDispatchToProps)(Header)
