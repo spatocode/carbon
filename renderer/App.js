8586029d76f4434dfc3fbcd281969a7734afa709
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-24 11:35:45 +0100

    convert to smart component and receive channel

--- renderer/App.js
+++ renderer/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { updateLibrary } from "./actions"
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Control from "./components/Control"
+const { ipcRenderer } = window.require("electron")
 
 class App extends Component {
+    componentWillMount () {
+        const { dispatch } = this.props
+        ipcRenderer.send("should-update", true)
+        ipcRenderer.on("update-library", (event, files) => {
+            dispatch(updateLibrary(files))
+        })
+    }
+
     render () {
         return (
             <div className="App">
@@ -15,4 +27,19 @@ class App extends Component {
     }
 }
 
-export default App
+App.propTypes = {
+    shouldUpdate: PropTypes.bool,
+    library: PropTypes.array
+}
+
+App.defaultProps = {
+    shouldUpdate: false,
+    library: []
+}
+
+const mapStateToProps = state => ({
+    shouldUpdate: state.media.shouldUpdate,
+    library: state.media.library
+})
+
+export default connect(mapStateToProps)(App)
