dce7952a7b366678cc15fb57eb976b886723b6e8
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-25 14:48:57 +0100

    indicate media library update

--- renderer/components/Header.jsx
+++ renderer/components/Header.jsx
@@ -4,13 +4,22 @@ import PropTypes from "prop-types"
 import { searchSong } from "../actions"
 import "./stylesheets/Header.scss"
 
-const Header = ({ handleSearch=f=>f }) => {
+const Header = ({ library=[], isUpdating=false, handleSearch=f=>f }) => {
+    const handleChange = (e) => {
+        // TODO: Write data mining algorithm
+    }
     return (
         <div className="Header">
+            <div className="update-library"
+                style={isUpdating ? { visibility: "visible" }
+                    : { visibility: "hidden" }}>
+                <div className="spinner-wrapper"></div>
+                <div>Updating library...</div>
+            </div>
             <div className="music-search">
                 <form onSubmit={handleSearch}>
                     <button type="submit" onClick={handleSearch}>S</button>
-                    <input type="search" placeholder="Search music" onChange={handleSearch} />
+                    <input type="search" placeholder="Search music" onChange={handleChange} />
                 </form>
             </div>
         </div>
@@ -22,8 +31,13 @@ Header.propTypes = {
     handleChange: PropTypes.func
 }
 
+const mapStateToProps = state => ({
+    isUpdating: state.media.isUpdating,
+    library: state.media.library
+})
+
 const mapDispatchToProps = dispatch => ({
     handleSearch: (e) => dispatch(searchSong(e.target.value))
 })
 
-export default connect(null, mapDispatchToProps)(Header)
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
