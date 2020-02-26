92c1dfc338dbda2bb49d87d79f8780964e0fa05b
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-02-26 15:15:13 +0100

    fix bug in tab styling

--- renderer/components/Tab.jsx
+++ renderer/components/Tab.jsx
@@ -6,7 +6,7 @@ const Tab = ({ tab="", selectTab=f=>f, titles=[], tabStyle={} }) => {
     return (
         <div className="tab">
             {titles.map((title, i) =>
-                <div key={i} className="tab-item" onClick={selectTab} style={tab==="All" ? tabStyle : null}>{title}</div>
+                <div key={i} className="tab-item" onClick={selectTab} style={tab===title ? tabStyle : null}>{title}</div>
             )}
         </div>
     )
