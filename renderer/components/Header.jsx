5642a9f09b495df54c681bddbb3e63e10af44a85
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-03-09 22:59:05 +0100

    change to class component

--- renderer/components/Header.jsx
+++ renderer/components/Header.jsx
@@ -4,40 +4,50 @@ import PropTypes from "prop-types"
 import { searchSong } from "../actions"
 import "./stylesheets/Header.scss"
 
-const Header = ({ library=[], isUpdating=false, handleSearch=f=>f }) => {
-    const handleChange = (e) => {
-        // TODO: Write data mining algorithm
+class Header extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            value: ""
+        }
     }
-    return (
-        <div className="Header">
-            <div className="update-library"
-                style={isUpdating ? { visibility: "visible" }
-                    : { visibility: "hidden" }}>
-                <div className="spinner-wrapper"></div>
-                <div>Updating library...</div>
+
+    handleSubmit () {
+        const { dispatch } = this.props
+        dispatch(searchSong(this.state.value))
+    }
+
+    handleChange (e) {
+        this.setState({ value: e.currentTarget.value })
+    }
+
+    render () {
+        const { isUpdating } = this.props
+        return (
+            <div className="Header">
+                <div className="update-library"
+                    style={isUpdating ? { visibility: "visible" }
+                        : { visibility: "hidden" }}>
+                    <div className="spinner-wrapper"></div>
+                    <div>Updating library...</div>
+                </div>
+                <div className="media-search">
+                    <form onSubmit={this.handleSubmit}>
+                        <span><button type="submit"></button></span>
+                        <input type="search" placeholder="Search media" onChange={this.handleChange} />
+                    </form>
+                </div>
             </div>
-            <div className="music-search">
-                <form onSubmit={handleSearch}>
-                    <button type="submit" onClick={handleSearch}>S</button>
-                    <input type="search" placeholder="Search music" onChange={handleChange} />
-                </form>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 Header.propTypes = {
-    handleSearch: PropTypes.func,
-    handleChange: PropTypes.func
+    isUpdating: PropTypes.bool
 }
 
 const mapStateToProps = state => ({
-    isUpdating: state.media.isUpdating,
-    library: state.media.library
+    isUpdating: state.media.isUpdating
 })
 
-const mapDispatchToProps = dispatch => ({
-    handleSearch: (e) => dispatch(searchSong(e.target.value))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header)
+export default connect(mapStateToProps, null)(Header)
