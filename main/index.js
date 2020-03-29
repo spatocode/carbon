a7c51244904624b30e707dc065ea179fff85caed
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-03-29 17:32:46 +0100

    change mediadata parsing lib
    Also change default window size. Handle creating/removing shortcuts on windows when installing/uninstalling using electron-squirrel-startup

--- main/index.js
+++ main/index.js
@@ -1,8 +1,14 @@
 const os = require("os")
 const path = require("path")
 const { app, BrowserWindow, ipcMain } = require("electron")
-const mi = require("mediainfo-wrapper")
+const mm = require("music-metadata")
+const klawSync = require("klaw-sync")
 const buildMenu = require("./menu")
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) { // eslint-disable-line global-require
+    app.quit()
+}
 
 let window
 
@@ -10,11 +16,11 @@ const createWindow = () => {
     window = new BrowserWindow({
         title: "Carbon Media Player",
         show: false,
-        width: 1000,
-        height: 600,
+        width: 860,
+        height: 500,
         useContentSize: true,
-        minHeight: 400,
-        minWidth: 800,
+        minHeight: 500,
+        minWidth: 860,
         webPreferences: {
             nodeIntegration: true,
             preload: path.join(__dirname, "/preload.js")
@@ -22,6 +28,7 @@ const createWindow = () => {
     })
 
     window.loadURL("http://localhost:3000")
+    // window.loadFile(path.join(__dirname, "../renderer/build/index.html"))
 
     buildMenu(window)
 
@@ -50,31 +57,46 @@ function fetchMedia () {
     })
 }
 
-function extractMediaInfo (musicDir) {
-    var files = []
-    mi({ maxBuffer: Infinity }, musicDir+"/*.mp3")
-        .then((data) => {
-            for (const i in data) {
-                files.push({
-                    file: data[i].file,
-                    file_name: data[i].general.file_name,
-                    file_size: data[i].general.file_size,
-                    duration: data[i].general.duration,
-                    title: data[i].general.title,
-                    album: data[i].general.album,
-                    artist: data[i].general.performer,
-                    genre: data[i].general.genre,
-                    date: data[i].general.recorded_date,
-                    cover_mime: data[i].general.cover_mime
-                    // cover_data: data[i].general.cover_data
+async function extractMediaInfo (musicDir) {
+    var metadata = []
+
+    // ignore hidden folders and non-mp3 files
+    const filterFunc = item => {
+        const basename = path.basename(item.path)
+        return basename === "." || basename[0] !== "." || (!item.stat.isDirectory() && path.extname(basename) !== ".mp3")
+    }
+
+    var files = klawSync(musicDir, { nodir: true, filter: filterFunc })
+    for (const file of files) {
+        var filepath = file.path
+        await mm.parseFile(filepath)
+            .then((data) => {
+                var dur
+                const common = data.common
+                const format = data.format
+                if (format.duration) {
+                    dur = `${Math.floor(format.duration/60)}:${Math.floor(format.duration%60)}`
+                } else {
+                    dur = "00:00"
+                }
+                metadata.push({
+                    file: filepath,
+                    file_name: path.basename(filepath, path.extname(filepath)),
+                    artist: common.artist,
+                    title: common.title,
+                    album: common.album,
+                    genre: common.genre,
+                    duration: dur,
+                    year: common.year,
+                    track: common.track.no,
+                    lyrics: common.lyrics
                 })
-            }
-        })
-        .finally(() => {
-            window.webContents.send("update-library", files)
-            console.log("DONE")
-        })
-        .catch((err) => console.log(err))
+                console.log(filepath)
+            })
+            .catch(err => console.error("ERROR:", err.message))
+    }
+    window.webContents.send("update-library", metadata)
+    console.log("DONE")
 }
 
 app.on("ready", createWindow)
