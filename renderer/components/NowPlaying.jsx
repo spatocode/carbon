98ea7f8d5e25bf3bbfbde0f39a939fb16d12e0de
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-03-29 17:36:07 +0100

    update nowplaying album art due to new parser
    Cache album art url to save load time

--- renderer/components/NowPlaying.jsx
+++ renderer/components/NowPlaying.jsx
@@ -1,30 +1,38 @@
 import React from "react"
 import { connect } from "react-redux"
 import "./stylesheets/NowPlaying.scss"
-const mi = window.require("mediainfo-wrapper")
+const mm = window.require("music-metadata")
+
+var cache = {}
 
 const NowPlaying = ({ media="", mode="Paused" }) => {
     const albumArt = React.createRef()
     let url
     if (media) {
-        // TODO: Cache url to reduce load time
-        mi({ maxBuffer: Infinity }, media)
-            .then((data) => {
-                url = `data:${data[0].general.cover_mime.toString()};base64,${data[0].general.cover_data.toString("base64")}`
-                var img = new Image(1, 1)
-                img.src = url
-                img.onerror = function (err) {
-                    console.log("Image error: ", err)
-                }
-                img.onload = function () {
-                    console.log("loaded")
-                    albumArt.current.style.backgroundImage = `linear-gradient(rgba(13, 13, 17, 0.7), rgba(13, 13, 17, 0.7)), url("${url}")`
-                }
+        if (cache.data && cache.data.file === media) {
+            url = cache.data.url
+            initImage(url, function () {
+                albumArt.current.style.backgroundImage = `linear-gradient(rgba(13, 13, 17, 0.7), rgba(13, 13, 17, 0.7)), url("${url}")`
             })
-            .finally(() => { console.log(url) })
-            .catch(() => {
-                // TODO: Fallback to a default Carbon Media Player icon
-            })
+        }
+        else {
+            mm.parseFile(media)
+                .then((data) => {
+                    let picture = data.common.picture
+                    if (picture) {
+                        picture = picture[0]
+                        url = `data:${picture.format};base64,${picture.data.toString("base64")}`
+                        cache.data = { file: media, url: url }
+                        initImage(url, function () {
+                            albumArt.current.style.backgroundImage = `linear-gradient(rgba(13, 13, 17, 0.7), rgba(13, 13, 17, 0.7)), url("${url}")`
+                        })
+                    }
+                })
+                .catch((err) => {
+                    console.log(err.message)
+                    // TODO: Fallback to a default Carbon Media Player icon
+                })
+        }
     }
     return (
         <div className="now-playing">
@@ -43,5 +51,14 @@ const mapStateToProps = (state) => ({
     media: state.media.current,
     mode: state.media.mode
 })
+
+function initImage (url, load) {
+    var img = new Image(1, 1)
+    img.src = url
+    img.onerror = function (err) {
+        console.log("Image error: ", err)
+    }
+    img.onload = load
+}
 
 export default connect(mapStateToProps)(NowPlaying)
