06c1e2ccbcbb9da6d54463408f1415f04939fd0d
Ekene Izukanne <ekeneizukanne@gmail.com>
2020-03-31 14:30:17 +0100

    fix typo in storing media player in memory

--- renderer/utils/index.js
+++ renderer/utils/index.js
@@ -1,14 +1,15 @@
 var app = { player: new Audio() }
 
-/*
- * Stores a DOM player to memory
+/**
+ * Stores a DOM media player in memory
+ * @param {Object} player
  */
 export function setPlayer (player) {
-    player.s = player
+    app.player = player
 }
 
 /*
- * Retrieves a DOM player to memory
+ * Retrieves a DOM media player from memory
  */
 export function getPlayer () {
     return app.player
